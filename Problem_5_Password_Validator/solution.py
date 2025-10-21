6a90709ec7d7e50fc5773697ba42574765444375
dejengetachew16-lgtm <dejengetachew16@gmail.com>
2025-10-21 11:52:08 +0300

    Update solution.py

--- Problem_5_Password_Validator/solution.py
+++ Problem_5_Password_Validator/solution.py
@@ -1,1 +1,8 @@
-# your code here
+name=input("your name ful name:")
+passward=input("enter your pasward:")
+length=len(p)
+#print(l)
+if length<8:
+    print("wrong pasward plece your paswared is must be greter than 8")
+if passward==name:
+    print("incorect")    
