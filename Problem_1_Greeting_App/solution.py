c3382e254755548adcaae647cc1aa8be0baef21c
dbu572 <dejgeta315@gmail.com>
2025-10-21 10:09:18 +0300

    project 1.py

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,7 @@
-# your code here
+name=input("enter your namme:")
+food=input("enter your favorit food:")
+year=int(input("enter curent year:"))
+print(f"wellcome {name}")
+age=int(input("enter your age:"))
+berth_year=year-age
+print(berth_year)
